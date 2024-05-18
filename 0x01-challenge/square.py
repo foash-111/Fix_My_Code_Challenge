6b1db87f6cccc08d82f6a4a9d54bf35374c2de15
foash-111 <mr2221@fayoum.edu.eg>
2024-05-18 19:03:12 +0300

    pycodestyle index.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,7 +12,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
