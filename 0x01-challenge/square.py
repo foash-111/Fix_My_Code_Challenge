02d2d4a4d6e50547fc5b0c4d15eb2fc03e61c4ce
foash-111 <mr2221@fayoum.edu.eg>
2024-05-18 19:00:51 +0300

    pycodestyle index.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+my square class
+"""
+
+
+class Square():
+    """class Square"""
+    def __init__(self, width=0,  height=0):
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -5,6 +5,7 @@ Web server
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
